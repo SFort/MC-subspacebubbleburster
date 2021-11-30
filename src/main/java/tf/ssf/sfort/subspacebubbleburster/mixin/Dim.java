b7425bbf435eb24d13f656669cf4a2c1add5be76
SFort <github@ssf.tf>
2021-11-30 21:32:59 +0100

    1.18

--- src/main/java/tf/ssf/sfort/subspacebubbleburster/mixin/Dim.java
+++ src/main/java/tf/ssf/sfort/subspacebubbleburster/mixin/Dim.java
@@ -13,7 +13,7 @@ public abstract class Dim {
 
 	@Shadow @Final protected static DimensionType THE_NETHER;
 
-	@Inject(method= "method_31109(Lnet/minecraft/world/dimension/DimensionType;Lnet/minecraft/world/dimension/DimensionType;)D", at=@At("HEAD"), cancellable = true)
+	@Inject(method= "getCoordinateScaleFactor", at=@At("HEAD"), cancellable = true)
 	private static void change_scale(DimensionType dimensionType, DimensionType dimensionType2, CallbackInfoReturnable<Double> cir){
 		if(dimensionType.equals(THE_NETHER)) cir.setReturnValue(Config.netherScale / dimensionType2.getCoordinateScale());
 		else if(dimensionType2.equals(THE_NETHER)) cir.setReturnValue(dimensionType.getCoordinateScale() / Config.netherScale);
